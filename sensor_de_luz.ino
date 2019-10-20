7ebaea22aac3dc05a8e4a79089468497e45e82a6
Ale2006 <54778764+Ale2006@users.noreply.github.com>
2019-10-20 12:58:53 -0300

    Add files via upload

---
+++ sensor_de_luz.ino
@@ -0,0 +1,20 @@
+int ldr1=0;
+int ldr2=1;
+
+void setup() {
+  pinMode (ldr1,INPUT);
+  pinMode (ldr2,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  double sensor1;
+  double sensor2;
+  sensor1= analogRead(ldr1);
+  sensor2= analogRead(ldr2);
+  Serial.print("Ambiente: ");
+  Serial.print(sensor2);
+   Serial.print("    Monitor: ");
+  Serial.println(sensor1);
+  delay(200);
+}
