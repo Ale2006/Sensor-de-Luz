c2ac646e447d5d1a30e6df28141aab93662b585a
Ale2006 <54778764+Ale2006@users.noreply.github.com>
2020-01-04 20:08:10 -0300

    Update sensor_de_luz.ino

--- sensor_de_luz.ino
+++ sensor_de_luz.ino
@@ -1,20 +1,55 @@
+/*
+ * ldr1 A0
+ * ldr2 A1
+ * Salida PWM D11  coenctado con R10k al Gate, entre Gate y Source R100k
+ * 
+ * 
+ *        masa
+ *       |
+ *       <
+ *       > R10k
+ *       |
+ * Señal-----\
+ *        ---/ LDR
+ *       |
+ *       5V
+*/
+
+int led=11;
 int ldr1=0;
 int ldr2=1;
+int valor=0;
+
 
 void setup() {
-  pinMode (ldr1,INPUT);
-  pinMode (ldr2,INPUT);
-  Serial.begin(9600);
+pinMode (led,OUTPUT);
+pinMode (ldr1, INPUT);
+pinMode (ldr2, INPUT);
+Serial.begin(9600);
 }
 
 void loop() {
-  double sensor1;
-  double sensor2;
-  sensor1= analogRead(ldr1);
-  sensor2= analogRead(ldr2);
-  Serial.print("Ambiente: ");
-  Serial.print(sensor2);
-   Serial.print("    Monitor: ");
-  Serial.println(sensor1);
-  delay(200);
+
+int lec1= analogRead (ldr1)/4;
+int lec2= analogRead (ldr2)/4;
+//Serial.print("Monitor (Ldr1): ");
+//Serial.println(lec1);
+//Serial.print("PWM: ");
+//Serial.println(valor);
+//Serial.print("Ambiente (Ldr2): ");
+//Serial.println(lec2);
+//delay(500);
+
+if(lec1<lec2 && valor>5){
+  valor--;
+  
+  }
+  else
+  {
+    if(valor<254)
+    valor++;
+   }
+
+analogWrite (led,valor);
+
 }
